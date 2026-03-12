ef3f44ff0926fa4d05485f3a0e7c69a0efbf7c96
fredrikBB <fredrik.b.bratberg@gmail.com>
2026-03-12 11:30:14 +0100

    Use the enable callback isntead of init_task
    Enable is only called for tasks actually registered for the scheduler, init_task is called for a bunch of other tasks which pollutes the trace_pipe

--- scheds/c/scx_edfvd.bpf.c
+++ scheds/c/scx_edfvd.bpf.c
@@ -14,18 +14,19 @@ struct {
 	__type(value, struct task_ctx);
 } task_ctx SEC(".maps");
 
-s32 BPF_STRUCT_OPS(edfvd_init_task, struct task_struct *p,
+s32 BPF_STRUCT_OPS(edfvd_enable, struct task_struct *p,
 		   struct scx_init_task_args *args)
 {
 	pid_t pid = p->pid;
-	bpf_printk("Initialzing task with pid %d\n", pid);
+	bpf_printk("Task with pid %d entered SCX\n", pid);
 	struct task_ctx *tctx = bpf_map_lookup_elem(&task_ctx, &pid);
 	if (tctx) {
 		bpf_printk(
-			"Task ctx for pid %d: task_nr=%llu, criticality= %d, period=%llu, modified_period=%llu, wcet_lo=%llu, wcet_hi=%llu\n",
-			p->pid, tctx->task_nr, tctx->criticality,
-			tctx->period_ms, tctx->modified_period_ms,
-			tctx->wcet_ms_lo, tctx->wcet_ms_hi);
+			"Task ctx for pid %d: task_nr=%llu, criticality=%s, period=%llu, modified_period=%llu, wcet_lo=%llu, wcet_hi=%llu\n",
+			p->pid, tctx->task_nr,
+			tctx->criticality == LO ? "LO" : "HI", tctx->period_ms,
+			tctx->modified_period_ms, tctx->wcet_ms_lo,
+			tctx->wcet_ms_hi);
 	} else {
 		bpf_printk("No task ctx found for pid %d\n", p->pid);
 	}
@@ -38,5 +39,5 @@ void BPF_STRUCT_OPS(edfvd_exit, struct scx_exit_info *ei)
 }
 
 SCX_OPS_DEFINE(edfvd_ops, .exit = (void *)edfvd_exit,
-	       .init_task = (void *)edfvd_init_task,
-	       .flags = SCX_OPS_SWITCH_PARTIAL, .name = "edfvd");
+	       .enable = (void *)edfvd_enable, .flags = SCX_OPS_SWITCH_PARTIAL,
+	       .name = "edfvd");
